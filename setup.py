7b982df80b66a340e4582e303d7f9d955c35c6f7
dermaxihalt <muellermax1985@gmail.com>
2020-05-17 17:30:24 +0200

    Upload to GitHub

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mmuda_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mmuda_distribution'],
+      author='Maximilian Müller',
+      zip_safe=False)
